06b5198c4a184b308c22eec590d9a94fd051b2f3
Елизавета <pozhidayevay@mail.ru>
2026-02-23 22:05:43 +0300

    add file with initial tasks and storage key

---
+++ data.js
@@ -0,0 +1,17 @@
+export const initialTasks = [
+    {
+        id: 1,
+        title: "Изучить JavaScript",
+        date: "2024-02-25",
+        completed: false
+    },
+    {
+        id: 2,
+        title: "Сделать лабораторную работу",
+        date: "2024-02-26",
+        completed: true
+    }
+];
+
+// Ключ для localStorage
+export const STORAGE_KEY = 'todo_tasks';
